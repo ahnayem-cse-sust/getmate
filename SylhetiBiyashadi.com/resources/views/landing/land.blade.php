7cb26457df1b5bf63c74f878bee66d3691555be8
Md-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2019-09-21 03:28:42 +0600

    footer design

--- SylhetiBiyashadi.com/resources/views/landing/land.blade.php
+++ SylhetiBiyashadi.com/resources/views/landing/land.blade.php
@@ -1,28 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        @foreach ($users as $user)
-            <div class="col-md-3">
-                <div class="row">
-                    <div class="col-md-12">
-                        <img width="40%" src="{{asset('demo.jpg')}}">
+    <section class="carosel-section">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="carosel-item-single" style="background-image: url({{ asset('img/banner1.jpg') }});">
                     </div>
-                    <div class="col-md-12">
-                        Name: {{@$user->name}}
+                </div>
+                <div class="carousel-item">
+                    <div class="carosel-item-single" style="background-image: url({{ asset('img/banner2.jpg') }});">
                     </div>
-                    @role('user')
-                    <div class="col-md-12">
-                        Height: {{@$user->userDetail->height}}
+                </div>
+                <div class="carousel-item">
+                    <div class="carosel-item-single" style="background-image: url({{ asset('img/banner3.jpg') }});">
                     </div>
-                    <div class="col-md-12">
-                        Religion: {{@$user->userDetail->religion}}
-                    </div>
-                    @endrole
                 </div>
             </div>
-        @endforeach
-    </div>
-</div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+
+    <section class="user-moving-section">
+aaa
+    </section>
+
+    <section class="user-section">
+        <div class="container">
+            <div class="row">
+                @foreach ($users as $user)
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img width="40%" src="{{asset('demo.jpg')}}">
+                        </div>
+                        <div class="col-md-12">
+                            Name: {{@$user->name}}
+                        </div>
+                        @role('user')
+                        <div class="col-md-12">
+                            Height: {{@$user->userDetail->height}}
+                        </div>
+                        <div class="col-md-12">
+                            Religion: {{@$user->userDetail->religion}}
+                        </div>
+                        @endrole
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    
+        
 @endsection
---
+++ SylhetiBiyashadi.com/resources/views/layouts/footer.blade.php
@@ -0,0 +1,36 @@
+
+<footer class="footer">
+
+    <div class="footer-quick-view py-4 py-md-5">
+        <div class="row m-0">
+            <div class="col-lg-3 col-md-6">
+                <h4 class="sub-title text-white text-center">Information</h4>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                    <h4 class="sub-title text-white text-center">Information</h4>
+                    <ul class="list-unstyled footer-menu">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Career</a></li>
+                        <li><a href="#">My Account</a></li>
+                    </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                    <h4 class="sub-title text-white text-center">Information</h4>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                    <h4 class="sub-title text-white text-center">Information</h4>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer-copyright py-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-left">Powerd by <a href="#">AwesomeTeam</a></div>
+            <div class="col-md-6 text-center text-md-right"><?php echo date("Y");?> &copy; SylhetiBiyashadi.com</div>
+            </div>
+        </div>
+    </div>
+
+</footer>
