b048a86b0ce8cc4dfec7e5eb0cacd5352d7bc843
Md-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2019-10-30 22:34:39 +0600

    footer

--- sylhetibiyashadi/resources/views/layouts/footer.blade.php
+++ sylhetibiyashadi/resources/views/layouts/footer.blade.php
@@ -1,33 +1,31 @@
 
 <footer class="footer">
-
-    <div class="footer-quick-view py-4 py-md-5">
+    <div class=" footer-quick-view py-3 py-md-4">
         <div class="row m-0">
             <div class="col-lg-3 col-md-6 text-center ">
-                <h4 class="sub-title text-white">Information</h4>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
             </div>
-            <div class="col-lg-3 col-md-6 text-lg-left text-center">
-                    <h4 class="sub-title text-white">Information</h4>
-                    <ul class="list-unstyled footer-menu">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Career</a></li>
-                        <li><a href="#">Latest News</a></li>
-                    </ul>
+            <div class="col-lg-3 col-md-6 text-center ">
+                <a href="#"><i class="fab fa-twitter"></i></a>
             </div>
-            <div class="col-lg-3 col-md-6 text-lg-right text-center">
-                    <h4 class="sub-title text-white">Quick Links</h4>
-                    <ul class="list-unstyled footer-menu">
-                        <li><a href="#">My Account</a></li>
-                        <li><a href="#">Search</a></li>
-                        <li><a href="#">Location</a></li>
-                        <li><a href="#">Best Match</a></li>
-                    </ul>
+            <div class="col-lg-3 col-md-6 text-center ">
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
             </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                    <h4 class="sub-title text-white ">News Letter</h4>
-                    <p>Enter your email and get updated informationa and Up to 20% discount about our event and best matched profile.</p>
+            <div class="col-lg-3 col-md-6 text-center ">
+                <a href="#"><i class="fab fa-youtube"></i></a>
             </div>
+        </div>
+    </div>
+    <div class="footer-menu py-5">
+        <div class="container">
+            <ul class="list-inline m-0 d-flex justify-content-center">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Story</a></li>
+                <li class="list-inline-item"><a href="#">Contact us</a></li>
+                <li class="list-inline-item"><a href="#">Login</a></li>
+                <li class="list-inline-item"><a href="#">Sign Up</a></li>
+            </ul>
         </div>
     </div>
     
@@ -35,7 +33,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-center text-md-left">Powerd by <a href="#">AwesomeTeam</a></div>
-            <div class="col-md-6 text-center text-md-right"><?php echo date("Y");?> &copy; SylhetiBiyashadi.com</div>
+            <div class="col-md-6 text-center text-md-right"> SylhetiBiyashadi.com &copy; <?php echo date("Y");?></div>
             </div>
         </div>
     </div>
